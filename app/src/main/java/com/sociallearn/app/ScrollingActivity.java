e2f55e30be744fad1d63e1fc2ce4b49e61bacb16
Sarath Hotspot <sarath.hotspot@gmail.com>
2016-07-17 06:56:46 +0530

    Fixing context there wrapper

--- app/src/main/java/com/sociallearn/app/ScrollingActivity.java
+++ app/src/main/java/com/sociallearn/app/ScrollingActivity.java
@@ -10,7 +10,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.internal.view.ContextThemeWrapper;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
